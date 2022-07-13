f1860da7d6d5c0858289be1fe9b2b914b64986d7
pabsanort2 <pabsanort2@alum.us.es>
2022-07-13 14:47:12 +0200

    F02: Añadidas páginas relacionadas con usuarios
    Añadidas las páginas de registro, login, página principal y una barra de navegación simple. Se pretenden mejorar todas estas dándole formato y añadiendo alguna funcionalidad a la página principal.

--- hongOS_project/hongOS_app/forms.py
+++ hongOS_project/hongOS_app/forms.py
@@ -1,7 +1,30 @@
 from django import forms
-from .models import Imagen
+from .models import Imagen, HongOSUser
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 class ImagenModelForm(forms.ModelForm):
     class Meta:
         model = Imagen
         fields = ('file_name',)
+
+class HongOSUserForm(forms.ModelForm):
+
+    class Meta:
+        model = HongOSUser
+        fields = (
+            'email',
+        )  #If we want all parameters we can just use fields = "_all_"
+        error_messages = {
+            'email': {
+                'unique':("Ya existe un usuario con este correo electrónico"),
+            },
+        }
+
+class UserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1','password2')
--- hongOS_project/hongOS_app/views.py
+++ hongOS_project/hongOS_app/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from fastbook import load_learner
 from pathlib import Path
 
 from .models import Imagen
-from .forms import ImagenModelForm
+from .forms import ImagenModelForm, HongOSUserForm, UserForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 
+@login_required
 def clasificar(request):
     if request.method == 'GET':
         form = ImagenModelForm(request.POST or None, request.FILES or None)
@@ -21,4 +28,44 @@ def clasificar(request):
             pred,pred_idx,probs = learner.predict(imagePath)
             return render(request, 'clasificar.html', {'imagePath': imagePath, 'predictedCategory': pred, 'method':request.method, 'prob': probs[pred_idx].item()*100})
 
-        
+def registro(request):
+    submitted = False
+    if request.method == "POST":
+        form1 = UserForm(request.POST)
+        form2 = HongOSUserForm(request.POST, request.FILES)
+        if form1.is_valid() and form2.is_valid():
+            form1.save()
+            gameetUser = form2.save(commit=False)
+            gameetUser.user = form1.save()
+            gameetUser.save()
+            messages.success(request, 'Tu cuenta se creó correctamente!')
+            return HttpResponseRedirect('/login')
+    else:
+        form1 = UserForm()
+        form2 = HongOSUserForm()
+    return render(request,'registro.html',{'form1': form1, 'form2': form2})
+
+def loginView(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'La sesión se inició correctamente')
+            return redirect('/home/')
+        else:
+            messages.info(request, 'El usuario o contraseña son incorrectos')
+            return redirect('/login/')
+    if request.method == 'GET':
+        return render(request, 'login.html',{})
+
+@login_required
+def home(request):
+    return render(request, 'home.html',{})
+
+def logoutView(request):
+    logout(request)
+    messages.success(request,'La sesión se cerró correctamente')
+    return redirect('/login/')
