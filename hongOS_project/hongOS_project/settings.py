aaa100932a8f2cad26d5bb0731d765137638f983
pabsanort2 <pabsanort2@alum.us.es>
2022-07-27 13:07:06 +0200

    Fix: Añadido el static root

--- hongOS_project/hongOS_project/settings.py
+++ hongOS_project/hongOS_project/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -128,6 +126,9 @@ LOGIN_URL = '/login'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
